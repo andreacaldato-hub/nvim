cbafff6e9e2d25df80b76225cc8c5f9462f986b4
andreacaldato-hub <andreacaldato6@gmail.com>
2025-03-10 20:17:45 +0100

    ciao

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,9 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
       vim.cmd.colorscheme("gruvbox-material")
 
-      vim.o.background = "dark" -- or "light" depending on preference
-
       -- Disable transparency
-      vim.g.gruvbox_material_transparent_background = 1
     end,
   },
 }
